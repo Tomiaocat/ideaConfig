6ba9dd9d41808a01e07a4063e4b68e6b6db781c5
tom <782454854@qq.com>
2019-04-15 20:15:13 +0800

    IU-2018.3.4 <double@DESKTOP-73HGMGU Create baseRefactoring.xml, project.default.xml, laf.xml, lombok-plugin.xml, other.xml, Class.java, jdk.table.xml, double_luo.xml, Default.xml, applicationLibraries.xml, path.macros.xml, find.xml, intentionSettings.xml, ui.lnf.xml, filetypes.xml, editor.codeinsight.xml, colors.scheme.xml, _@user_Darcula.icls, editor.xml, usageView.xml, mavenVersion.xml, templates.xml, debugger.xml, updates.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,13 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+* @program: ${PROJECT_NAME}
+*
+* @description: ${description}
+*
+* @author: double
+*
+* @create: ${YEAR}-${MONTH}-${DAY} ${HOUR}:${MINUTE}
+**/
+public class ${NAME} {
+}
